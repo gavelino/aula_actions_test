7d2c4880fcbcd5eef6ca8855bf3672a0e300fab4
Guilherme Avelino <gavelino@gmail.com>
2025-11-03 17:44:19 -0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learssn React
+          Learn React
         </a>
       </header>
     </div>
